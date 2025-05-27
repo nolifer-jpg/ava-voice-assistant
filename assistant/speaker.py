73ebba78d615ca207882df8e3589b279a6a9dabe
nolifer_jpg <roysouhard2706@gmail.com>
2025-05-27 15:22:42 +0530

    update

--- assistant/speaker.py
+++ assistant/speaker.py
@@ -1,1 +1,13 @@
- 
+import pyttsx3
+
+def speak(text):
+    newVoiceRate = 145
+    engine = pyttsx3.init()
+    engine.setProperty('rate',newVoiceRate)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+
+speak("Ava is online.")    
